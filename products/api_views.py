33da930d1e29954195a38c2188aa598d16e52b8e
RealRick37 <122051997+RealRick37@users.noreply.github.com>
2026-07-14 12:08:17 +0330

    Add DRF permissions for products API

--- products/api_views.py
+++ products/api_views.py
@@ -1,11 +1,10 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
+# from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .permissions import IsAdminOrReadOnly
 
 class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
+    permission_classes=[IsAdminOrReadOnly]
